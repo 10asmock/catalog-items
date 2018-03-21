a578cf900209d3672a524d763dec1f816b7f681f
10asmock <hegemon1984@gmail.com>
2018-03-21 18:41:27 +0000

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -34,6 +34,7 @@ class Category(Base):
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+    catalog_item = relationship('CatalogItem', cascade='all, delete-orphan')
 
     @property
     def serialize(self):
