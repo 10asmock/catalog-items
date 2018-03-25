4abc6ccf010b0be601d2b1172013e366bedb6454
10asmock <hegemon1984@gmail.com>
2018-03-25 13:02:05 -0700

    Update itemcatalog.py

--- itemcatalog.py
+++ itemcatalog.py
@@ -95,77 +95,72 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(
-            '',
-            json.dumps(Current user is already connected, 200)
-            response.headers['Content-Type']='application/json'
+        response = make_response(json.dumps('User is already connected.'), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+# Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+        login_session['user_id'] = user_id
+
+    return output
+
+    # User Helper Functions
+    def createUser(login_session):
+        newUser = User(name=login_session['username'],
+                       email=login_session['email'],
+                       picture=login_session['picture'])
+        session.add(newUser)
+        session.commit()
+        user = session.query(User).filter_by(email=login_session['email'])
+        .one()
+        return user.id
+
+    def getUserInfo(user_id):
+        user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+    def getUserID(email):
+        try:
+            user = session.query(User).filter_by(email=email).one()
+    return user.id
+    except:
+        return None
+
+    @app.route('/gdisconnect')
+    def gdisconnect():
+        access_token = login_session.get('access_token')
+        if access_token is None:
+            print 'Access Token is None'
+            response = make_response(
+                    json.dumps('Current user not connected.'), 401)
+            response.headers['Content-Type'] = 'application/json'
             return response
-
-            # Store the access token in the session for later use.
-            login_session['access_token']=credentials.access_token
-            login_session['gplus_id']=gplus_id
-            # Get user info
-            userinfo_url="https://www.googleapis.com/oauth2/v1/userinfo"
-            params={'access_token': credentials.access_token, 'alt': 'json'}
-            answer=requests.get(userinfo_url, params=params)
-
-            data=answer.json()
-
-            login_session['username']=data['name']
-            login_session['picture']=data['picture']
-            login_session['email']=data['email']
-
-            user_id=getUserID(login_session['email'])
-            if not user_id:
-                user_id=createUser(login_session)
-                login_session['user_id']=user_id
-
-            return output
-
-            # User Helper Functions
-            def createUser(login_session):
-                newUser=User(name=login_session['username'],
-                             email=login_session['email'],
-                             picture=login_session['picture'])
-                session.add(newUser)
-                session.commit()
-                user=session.query(User).
-                filter_by(email=login_session['email']).one()
-                return user.id
-
-
-                def getUserInfo(user_id):
-                    user=session.query(User).filter_by(id=user_id).one()
-                    return user
-
-
-                def getUserID(email):
-                    try:
-                    user=session.query(User).filter_by(email=email).one()
-                    return user.id
-                    except:
-                    return None
-
-
-                @app.route('/gdisconnect')
-                def gdisconnect():
-                    access_token=login_session.get('access_token')
-                if access_token is None:
-                    print 'Access Token is None'
-                    response=make_response(
-                        json.dumps('Current user not connected.'), 401)
-                    response.headers['Content-Type']='application/json'
-                    return response
-                    print 'In gdisconnect access token is %s', access_token
-                    print 'User name is: '
-                    print login_session['username']
-                    url='https://accounts.google.com/o/oauth2/revoke?token=%s'
-                    % login_session['access_token']
-                    h=httplib2.Http()
-                    result=h.request(url, 'GET')[0]
-                    print 'result is '
-                    print result
-                    if result['status'] == '200':
+            print 'In gdisconnect access token is %s', access_token
+            print 'User name is: '
+            print login_session['username']
+            url = 'https://accounts.google.com/o/oauth2/revoke?token=%s'
+            % login_session['access_token']
+            h = httplib2.Http()
+            result = h.request(url, 'GET')[0]
+            print 'result is '
+            print result
+            if result['status'] == '200':
                         del login_session['access_token']
                         del login_session['gplus_id']
                         del login_session['username']
@@ -401,6 +396,6 @@ def gconnect():
 
 
                     if __name__ == '__main__':
-                        app.secret_key='super_secret_key'
-                        app.debug=True
+                        app.secret_key = 'super_secret_key'
+                        app.debug = True
                         app.run(host='0.0.0.0', port=5000)
